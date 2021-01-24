92550f23cfdd3596154681e2d4d46be99c2dcc93
XOCO96 <lalo_1010_@hotmail.com>
2021-01-24 17:55:14 -0600

    Archivo xml y kt renombrados para el panel de logeo de la aplicación y agregados campos de texto y botones para logeo básico al archivo gráfico

--- app/src/main/java/com/xoco96/pzplayv1/LogActivity.kt
+++ app/src/main/java/com/xoco96/pzplayv1/LogActivity.kt
@@ -3,9 +3,9 @@ package com.xoco96.pzplayv1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_log)
     }
 }
