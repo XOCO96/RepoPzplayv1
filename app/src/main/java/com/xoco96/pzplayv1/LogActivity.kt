125df9541343036eefa4475e24259eaf1f32dfa1
XOCO96 <lalo_1010_@hotmail.com>
2021-01-24 22:35:07 -0600

    Evento personalizado -InitScreen- para Google Analytics incorporado para recopilacion de datos mediante Firebase

--- app/src/main/java/com/xoco96/pzplayv1/LogActivity.kt
+++ app/src/main/java/com/xoco96/pzplayv1/LogActivity.kt
@@ -2,6 +2,7 @@ package com.xoco96.pzplayv1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class LogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +11,11 @@ class LogActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log)
+
+        // Evento personalizado para Google Analytics
+        val analytics :FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Integración de Firebase completa")
+        analytics.logEvent("InitScreen",bundle)
     }
 }
