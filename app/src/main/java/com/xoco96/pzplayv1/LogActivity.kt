f96264032aae6765b49dee54ac477a71a3097e18
XOCO96 <lalo_1010_@hotmail.com>
2021-01-24 20:27:07 -0600

    Personalización de los botones y campos de texto anteriormente agregados, incorporado splashscreen

--- app/src/main/java/com/xoco96/pzplayv1/LogActivity.kt
+++ app/src/main/java/com/xoco96/pzplayv1/LogActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 
 class LogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        setTheme(R.style.AppTheme)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log)
     }
