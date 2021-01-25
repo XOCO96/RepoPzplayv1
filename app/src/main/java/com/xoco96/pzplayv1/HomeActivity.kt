b192bd5defc23ff1cd5bd584ea402b5608766b01
XOCO96 <lalo_1010_@hotmail.com>
2021-01-25 16:43:22 -0600

    Agregada parte lógica del activity Home para mostrar la informacion del usuario registrado/logeado y bonton -Cerrar sesión-

--- app/src/main/java/com/xoco96/pzplayv1/HomeActivity.kt
+++ app/src/main/java/com/xoco96/pzplayv1/HomeActivity.kt
@@ -2,6 +2,8 @@ package com.xoco96.pzplayv1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_home.*
 
 enum class ProviderType{
     BASIC
@@ -11,5 +13,26 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        // Configuración: se extraen los valores recibidos por el activity Log
+        // y se envían a la funcion setup
+        val bundle: Bundle? = intent.extras
+        val email: String? = bundle?.getString("email")
+        val provider: String? = bundle?.getString("provider")
+        setup(email?: "",provider?:"")
+    }
+
+    //Función que muestra en pantalla el correo y provedor mediante el cual
+    // el usuario se ha registrado
+    private fun setup(email: String, provider: String){
+        title = "Inicio"
+
+        emailTextView.text = email
+        providerTextView.text = provider
+
+        logoutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
     }
 }
